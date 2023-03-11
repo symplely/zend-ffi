32433876be5a25e2a32e5ad223d1ce78d61a6c51
Lawrence Stubbs <lstubbs@techno.express>
2023-03-11 09:41:29 -0500

    update PThread.php

--- zend/Types/PThread.php
+++ zend/Types/PThread.php
@@ -4,10 +4,38 @@ declare(strict_types=1);
 
 namespace ZE;
 
+use Closure;
 use ZE\Thread;
+use FFI\CData;
+use ZE\Zval;
 
 if (\PHP_ZTS && !\class_exists('PThread')) {
-    final class PThread extends Thread
+    final class PThread
     {
+        /** @var \php_thread */
+        private ?\CStruct $thread = null;
+
+        private ?Closure $func = null;
+        private ?\ThreadsModule $module = null;
+
+        public function get_module(): ?\ThreadsModule
+        {
+            return $this->module;
+        }
+
+        public function __invoke(): ?CData
+        {
+            return $this->thread->__invoke();
+        }
+
+        public function __construct(\ThreadsModule $module)
+        {
+            $this->module = $module;
+            $this->thread = \c_typedef('php_thread', 'ts');
+        }
+
+        public function execute()
+        {
+        }
     }
 }
